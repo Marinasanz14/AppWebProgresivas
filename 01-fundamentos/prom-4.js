a6d6a58c8cf746ff89bc100e3ccd0561652d3419
Marina Sanchez <masaro1411@gmail.com>
2022-09-27 18:07:10 -0500

    Promise Race

---
+++ 01-fundamentos/prom-4.js
@@ -0,0 +1,37 @@
+
+
+function sumarLento( numero ){
+
+    return new Promise( function(resolve, reject){
+
+        setTimeout( function(){
+            resolve( numero +1);
+        }, 800);
+
+    })
+}
+
+
+let sumarRapido  = (numero) => {
+
+    return new Promise ( (resolve, reject) => {
+
+        setTimeout( () => {
+        //resolve( numero+1 );
+        reject('Error en sumar rapido')
+    } , 1000); 
+
+    })
+}
+
+
+Promise.race([ sumarLento(5), sumarRapido(10)])
+    .then( respuesta => {
+        console.log(respuesta);
+    })
+    .catch( console.log);
+
+
+
+//sumarLento( 5).then ( console.log);
+//sumarRapido( 10 ).then ( console.log);
